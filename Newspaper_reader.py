4f78d5c93a8ee3ab8961346b6655947fd37ecc57
aarush_2410 <54847424+aarush2786@users.noreply.github.com>
2020-12-26 00:00:05 +0530

    Update Newspaper_reader.py

--- Newspaper_reader.py
+++ Newspaper_reader.py
@@ -1,7 +1,6 @@
 import requests
 import time
 import json
-import win32com.client
 import platform
 def speak(str):
     speak=win32com.client.Dispatch("SAPI.SpVoice")
@@ -11,11 +10,11 @@ try:
     response = requests.get(url)
     a = response.json()
     p = a['articles']
-    print("Performing Recognitions for Compatibilty...")
-    speak("Performing Recognitions for Compatibilty...")
+    print(f"Performing Recognitions for Compatibilty...")
+    speak(f"Performing Recognitions for Compatibilty...")
     time.sleep(2)
-    speak("Your Operating System is , {platform.platform()}. And your Hardware is ,{platform.machine()}")
-    print("Your OS: {platform.platform()} \nHardware:{platform.machine()}")
+    speak(f"Your Operating System is , {platform.platform()}. And your Hardware is ,{platform.machine()}")
+    print(f"Your OS: {platform.platform()} \nHardware:{platform.machine()}")
     time.sleep(2)
     speak("Loading the Virtual News assistant. Please, wait Sir!")
     print("Loading the Virtual News assistant Please wait Sir!")
@@ -29,24 +28,24 @@ try:
     speak("Please Press ENTER to start-----")
     inp=input("Please Press ENTER to start-----")
 except Exception as e:
-    print("Performing Recognitions for Compatibilty...")
+    print(f"Performing Recognitions for Compatibilty...")
     time.sleep(2)
-    print("Your OS: {platform.platform()} \nHardware:{platform.machine()}")
+    print(f"Your OS: {platform.platform()} \nHardware:{platform.machine()}")
     print("Loading the Virtual News assistant Please wait Sir!")
     time.sleep(2)
     print("Checking internet connections")
     print("Check your Network connection Sir! I think you are having some problem with the internet either too slow or broken network ")
     print("ERROR: 404  NOT FOUND ")
-if input=="" \
+if inp=="" \
         "":
     time.sleep(1)
     for i in range(len(p)):
-        print("News number {i + 1}")
-        speak("News number {i + 1} about {p[i]['description']}")
+        print(f"News number {i + 1}")
+        speak(f"News number {i + 1} about {p[i]['description']}")
         time.sleep(1)
         speak(p[i]['content'])
         time.sleep(1)
-        speak("For more information kindly visit {p[i]['url'].split('//')[1][:40]}")
-        print("For more information kindly visit {p[i]['url'][:40]}")
+        speak(f"For more information kindly visit {p[i]['url'].split('//')[1][:40]}")
+        print(f"For more information kindly visit {p[i]['url'][:40]}")
         print("\n")
         time.sleep(2)
